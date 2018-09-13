fe86885bcf1beec168d15f44c9ac7d97c223b7e0
KamilSobien <kamil.sobien@gmail.com>
2018-09-13 11:01:48 +0200

    Create a gig model

--- GigHub/Models/ApplicationUser.cs
+++ GigHub/Models/ApplicationUser.cs
@@ -14,13 +14,5 @@ namespace GigHub.Models {
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false) {
-        }
 
-        public static ApplicationDbContext Create() {
-            return new ApplicationDbContext();
-        }
-    }
 }
