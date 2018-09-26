e85f5efde1608d2d5e139262cdd66580c265dfb2
KamilSobien <kamil.sobien@gmail.com>
2018-09-26 18:34:16 +0200

    bug fixing

--- GigHub/Controllers/API/FollowingController.cs
+++ GigHub/Controllers/API/FollowingController.cs
@@ -17,7 +17,7 @@ namespace GigHub.Controllers {
         public IHttpActionResult Follow(FollowingDto dto) {
             var userId = User.Identity.GetUserId();
 
-            if (_context.Followings.Any(f => f.FolloweeId == userId && f.FolloweeId == userId)) {
+            if (_context.Followings.Any(f => f.FolloweeId == userId && f.FolloweeId == dto.FolloweeId)) {
                 return BadRequest("Following already exists.");
             }
 
--- GigHub/ViewModels/GigsViewModel.cs
+++ GigHub/ViewModels/GigsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using GigHub.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GigHub.ViewModels {
     public class GigsViewModel {
@@ -7,6 +8,6 @@ namespace GigHub.ViewModels {
         public bool ShowActions { get; set; }
         public string Heading { get; set; }
         public string SearchTerm { get; set; }
-
+        public ILookup<int, Attendance> Attendances { get; set; }
     }
 }
