693c810aa243d118dcd99a9391156f3584cf2bd4
KamilSobien <kamil.sobien@gmail.com>
2018-09-26 19:47:07 +0200

    modularizing javascript

---
+++ GigHub/Scripts/app/controllers/gigsController.js
@@ -0,0 +1,49 @@
+﻿var GigsController = function (attendanceService) {
+    var button;
+    var init = function (container) {
+        $(container).on("click", ".js-toggle-attendance", toggleAttendance);
+    };
+
+    var toggleAttendance = function (e) {
+        button = $(e.target);
+        var gigId = button.attr("data-gig-id");
+        if (button.hasClass("btn-default")) {
+            attendanceService.createAttendance(gigId, done, fail);
+        } else {
+            attendanceService.deleteAttendance(gigId, done, fail);
+        }
+    };
+
+
+
+    var fail = function () {
+        alert("Something failed");
+    };
+
+    var done = function () {
+        var text = (button.text() == "Going") ? "Going?" : "Going";
+
+        button.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    };
+
+
+
+    var follow = function () {
+        $(".js-toggle-follow").click(function (e) {
+            var button = $(e.target);
+            $.post("/api/following", { followeeId: button.attr("data-user-id") })
+                .done(function () {
+                    button.text("Following");
+                })
+                .fail(function () {
+                    alert("Something failed");
+                });
+        });
+    };
+
+    return {
+        init: init,
+        follow: follow
+    };
+
+}(AttendanceService);
